1cab8cac9cbbed64c14996ea02e04fd348e13f1f
Platon <soulshon@mail.ru>
2026-07-13 08:10:17 +0300

    corrected enum files with products name

--- src/test/java/enums/ProdutsTitlesNaming.java
+++ src/test/java/enums/ProdutsTitlesNaming.java
@@ -3,11 +3,10 @@ package enums;
 public enum ProdutsTitlesNaming {
     BACKPACK("Sauce Labs Backpack"),
     TSHIRT("Sauce Labs Bolt T-Shirt"),
-    ONISE ("Sauce Labs Onesie"),
+    ONISE("Sauce Labs Onesie"),
     LIGHT("Sauce Labs Bike Light"),
     JACKET("Sauce Labs Fleece Jacket"),
     REDTSHIRT("Test.allTheThings() T-Shirt (Red)");
-
 
     private final String displayName;
 
